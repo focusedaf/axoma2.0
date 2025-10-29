1cf36e3f27be26a61b39a9c9bea54e1ac9223a52
focusedaf <andbeyondinfinity66@gmail.com>
2025-10-29 10:53:22 +0530

    stuff

---
+++ primary-server/src/routes/preExam.ts
@@ -0,0 +1,21 @@
+import express, { Response, NextFunction } from "express";
+import { authMiddleware, AuthenticatedRequest } from "../middleware/auth";
+import { upload } from "../utils/cloudinaryUpload";
+import { uploadPreExamImage } from "../controllers/preExam";
+
+const UploadRouter = express.Router();
+
+UploadRouter.post(
+  "/pre-exam-image",
+  authMiddleware,
+  upload.single("image"), // matches formData.append("image", blob)
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    try {
+      await uploadPreExamImage(req, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default UploadRouter;
--- primary-server/src/server.ts
+++ primary-server/src/server.ts
@@ -8,6 +8,8 @@ import profileRouter from "./routes/profile";
 import docVerificationRouter from "./routes/docVerification";
 import phoneVerificationRouter from "./routes/phoneVerification";
 import AiRouter from "./routes/ai";
+import UploadRouter from "./routes/preExam";
+
 const app = express();
 const PORT = 4000;
 
@@ -27,16 +29,14 @@ app.use(cookieParser());
 
 // APIs
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/verify-phone", phoneVerificationRouter); 
-app.use("/api/v1/user", profileRouter); 
+app.use("/api/v1/verify-phone", phoneVerificationRouter);
+app.use("/api/v1/user", profileRouter);
 app.use("/api/v1/verify-docs", docVerificationRouter);
-app.use("/api/v1",AiRouter)
+app.use("/api/v1", AiRouter);
+app.use("/api/v1/upload", UploadRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Axoma 2.0 backend is live " });
 });
 
-app.listen(PORT,  () =>
-  console.log(`Backend running on :${PORT}`)
-);
-
+app.listen(PORT, () => console.log(`Backend running on :${PORT}`));
